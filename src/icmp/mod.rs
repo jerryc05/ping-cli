c5e87d9b9ebdb844ddd4adf6f78cd6927cf5722b
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-04-14 17:48:23 -0400

    restructure

---
+++ src/icmp/mod.rs
@@ -0,0 +1,5 @@
+pub mod icmp;
+pub mod icmp_0_type;
+pub mod icmp_1_code;
+pub mod icmp_2_checksum;
+pub mod known_structs;
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
 pub mod icmp;
-pub mod icmp_0_type;
-pub mod icmp_1_code;
-pub mod icmp_2_checksum;
 mod utils;
-pub mod known_icmp_structs;
-pub mod icmp_sender;
+pub mod io;
