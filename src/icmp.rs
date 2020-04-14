ef85883ca0f73ed5bd80d805d8396c69b2d979b1
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-04-13 22:52:44 -0400

    start of impl

--- src/icmp.rs
+++ src/icmp.rs
@@ -2,6 +2,7 @@ use crate::icmp_type::IcmpType;
 use crate::icmp_code::IcmpCode;
 use crate::icmp_checksum::IcmpChecksum;
 
+/// [RFC 792](https://tools.ietf.org/html/rfc792)
 pub struct Icmp{
   pub type_:IcmpType,
   pub code: IcmpCode,
