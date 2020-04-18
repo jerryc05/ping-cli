8b460e491d6536bfb2a5135337ee7fa7d31b6d66
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-04-18 02:44:38 -0400

    minor tweak; add print output

--- src/io/icmp_sender.rs
+++ src/io/icmp_sender.rs
@@ -5,6 +5,7 @@ use socket2::{Socket, Domain, Type, Protocol};
 use std::net::{SocketAddr, IpAddr};
 use std::ops::Deref;
 use std::time::{Duration, Instant};
+use std::borrow::Cow;
 
 const DEFAULT_TIMEOUT: Option<Duration> = Some(Duration::from_secs(4));
 const DEFAULT_TTL: u32 = 64;
@@ -80,7 +81,7 @@ pub fn ping(addr: IpAddr, timeout_opt: PingTimeout,
       }
     };
     socket.set_read_timeout(timeout).map_err(
-      |err|MyErr::from( (&err, file!(), line!() - 1)))?;
+      |err| MyErr::from((&err, file!(), line!() - 1)))?;
 
     /* checksum */
     {
@@ -116,15 +117,28 @@ pub fn ping(addr: IpAddr, timeout_opt: PingTimeout,
 
   /* recv */{
     let (size, addr) = socket.recv_from(&mut recv_buf).map_err(
-      |err| MyErr::from((&err, file!(), line!())))?;
+      |err| MyErr::from((&err, file!(), line!() - 1)))?;
     duration = timer.elapsed();
-    dbg!(size, addr, duration);
     debug_assert_eq!(size, recv_buf.len());
 
     for b in &recv_buf[IPV4_HEADER_SIZE..size] {
       print!("{:02x} ", *b);
     }
     println!();
+    println!("64 bytes from 93.184.216.34: icmp_seq=0 ttl=56 time=11.632 ms");
+    println!("{} bytes from {}: icmp_seq={} ttl={} time={:.3} ms",
+             "??",
+             addr.as_std().map_or_else(
+               || Cow::from("??"),
+               |std_addr| std_addr.ip().to_string().into()),
+             "??",
+             socket.ttl().map_or_else(
+               |err| {
+                 eprintln!("{:?}", MyErr::from((&err, file!(), line!() - 2)));
+                 Cow::from("??")
+               },
+               |ttl| ttl.to_string().into()),
+             duration.as_secs_f32() * (Duration::from_secs(1).as_millis() as f32));
   }
 
   // let reply =
